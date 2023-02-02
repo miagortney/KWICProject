3df69d8a66f02eb9a4923a94cdd6bc820a3c9cdc
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2023-02-01 19:45:30 -0500

    feat: shift lines with CircularShifter object

--- src/main/java/CircularShifter.java
+++ src/main/java/CircularShifter.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class CircularShifter {
-
+    ArrayList<String[]> shiftedLines;
     public void setup(LineStorage lines) {
+        shiftedLines = new ArrayList<String[]>();
+        for (String line: lines.lines){
+            String[] lineWords = line.split(" ");
+            for (int i = 0; i < lineWords.length; i++) {
+                String[] shiftedLine = new String[lineWords.length];
+                getShiftedLine(lineWords, shiftedLine, i);
+                shiftedLines.add(shiftedLine);
+            }
+        }
+    }
 
+    private void getShiftedLine(String[] src, String shifted[], int startIndex){
+        for(int i = startIndex; i < startIndex + src.length; i++){
+            shifted[i - startIndex] = src[i % src.length];
+        }
     }
 }
