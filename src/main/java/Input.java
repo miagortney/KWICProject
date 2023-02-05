d10f13a43723d985d0ba77ac3ea66a98512f4c4b
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2023-02-05 15:32:14 -0500

    fix: String file field as file path

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Input {
 
-    public void parse(String file, LineStorage lines) {
-        lines.lines = file.split("\n");
+    public void parse(String file, LineStorage lines) throws FileNotFoundException {
+
+        // pass the path to the file as a parameter
+        File f = new File(file);
+        Scanner sc = new Scanner(f);
+        lines.lines = new ArrayList<String>();
+        while (sc.hasNextLine())
+            lines.lines.add(sc.nextLine());
     }
 }
